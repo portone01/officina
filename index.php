d362d61fed2b7909b6e7c1b90545d29cee7d9597
viola <viola@10.21.100.228>
2023-05-27 10:29:18 +0200

    commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Officina Alfa</title>
+        <link rel="icon" href="Immagini/iconaSito.png" type="image/x-icon"/>
+        <link rel="shortcut icon" href="Immagini/iconaSito.png" type="image/x-icon"/>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <ul>
+            <div class="hamburger-menu">
+                <input id="menu__toggle" type="checkbox" />
+                <label class="menu__btn" for="menu__toggle">
+                    <span></span>
+                </label>
+
+                <ul class="menu__box">
+                    <li><a class="menu__item" href="#">Home</a></li>
+                    <li><a class="menu__item" href="#">About</a></li>
+                    <li><a class="menu__item" href="#">Team</a></li>
+                    <li><a class="menu__item" href="#">Contact</a></li>
+                    <li><a class="menu__item" href="#">Twitter</a></li>
+                </ul>
+            </div>
+            <li class="linea"><a class="navbar" href="accesso.php">Accedi</a></li>
+            <li class="linea"><a class="navbar" href="registrazione.php">Registrati</a></li>
+        </ul>
+        <?php
+        ?>
+    </body>
+</html>
